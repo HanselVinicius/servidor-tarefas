298d499165bf1b95b4b0f152932147950507c13f
Vinicius Hansel <hanelvinicius@gmail.com>
2023-06-15 13:41:59 -0300

    volatile/atomic

--- src/br/com/servidor/ServidorTarefas.java
+++ src/br/com/servidor/ServidorTarefas.java
@@ -6,18 +6,20 @@ import java.net.Socket;
 import java.net.SocketException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ServidorTarefas {
 
     private final ServerSocket servidor;
     private final ExecutorService threadPool;
-    private boolean estaRodando;
+    // volatile e atmoics são para permitir que as threads não salvem os valores destas variaveis em seu cache local
+    private AtomicBoolean estaRodando;
 
     public ServidorTarefas() throws IOException {
         System.out.println("-------- Iniciando Servidor  -------- ");
          this.servidor = new ServerSocket(12345);
         this.threadPool = Executors.newCachedThreadPool();
-        this.estaRodando = true;
+        this.estaRodando = new AtomicBoolean(true);
     }
 
     public static void main(String[] args) throws Exception {
@@ -30,13 +32,13 @@ public class ServidorTarefas {
     }
 
     public void parar() throws IOException {
-        this.estaRodando = false;
+        this.estaRodando.set(false);
         servidor.close();
         threadPool.shutdown();
     }
 
     public void rodar() throws IOException {
-        while(this.estaRodando) {
+        while(this.estaRodando.get()) {
             try {
                 Socket socket = servidor.accept();
                 System.out.println(" Aceitando novo cliente na porta :" + socket.getPort());
